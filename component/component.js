4a9b60eadc9c785e995df8216503f9794016526c
Radu Stefanache <radu.stefanache@gmail.com>
2016-11-10 20:20:30 +0000

    Whitespaces
    Easier to read.

--- component/component.js
+++ component/component.js
@@ -11,8 +11,8 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 				apikey: '',
 				secretkey: '',
 				zoneid: null,
-                                vdcregion: '',
-                                networkid: null,
+        vdcregion: '',
+        networkid: null,
 				templateid: null,
 				serviceofferingid: null,
 				diskofferingid: null,
@@ -25,169 +25,168 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 			}));
 
 			let regions = [ { "id": "Asia", "name": "Asia" }, { "id": "Europe", "name": "EU" }, { "id": "USA", "name": "US" } ];
-                        this.set('avregions', regions);
+        this.set('avregions', regions);
 		},
 		actions: {
-		    cloudAuth: function() {
-			    this.set('step', 2);
-			    this.apiRequest('listZones').then((res) => {
-		            let zones = [];
-		            res.listzonesresponse.zone.forEach((zone) => {
-		                let obj = {
-		                    id: zone.id,
-		                    name: zone.name
-		                };
-		                zones.push(obj);
-		            });
-		            this.set('avzones', zones);
-		            this.set('step', 3);
-		        }, (err) => {
-		            let errors = this.get('errors') || [];
-		            errors.pushObject(this.apiErrorMessage(err, '', '', 'Authentication failure'));
-		            this.set('errors', errors);
-		            this.set('step', 1);
-		        });
-		    },
+		  cloudAuth: function() {
+			  this.set('step', 2);
+			  this.apiRequest('listZones').then((res) => {
+		      let zones = [];
+		      res.listzonesresponse.zone.forEach((zone) => {
+		        let obj = {
+		          id: zone.id,
+		          name: zone.name
+		        };
+		        zones.push(obj);
+		      });
+		      this.set('avzones', zones);
+		      this.set('step', 3);
+		    }, (err) => {
+		      let errors = this.get('errors') || [];
+		      errors.pushObject(this.apiErrorMessage(err, '', '', 'Authentication failure'));
+		      this.set('errors', errors);
+		      this.set('step', 1);
+		    });
+		  },
 
-		    selectZone: function() {
-			this.set('step', 4);
-			this.apiRequest('listNetworks', { zoneid: this.get('model.%%DRIVERNAME%%Config.zoneid') }).then((res) => {
+		  selectZone: function() {
+			  this.set('step', 4);
+			  this.apiRequest('listNetworks', { zoneid: this.get('model.%%DRIVERNAME%%Config.zoneid') }).then((res) => {
 			    let networks = [];
 			    (res.listnetworksresponse.network || []).forEach((net) => {
-			        let obj = {
-			            id: net.id,
-			            name: net.name,
-			        };
-			        networks.push(obj);
+			      let obj = {
+			        id: net.id,
+			        name: net.name,
+			      };
+			      networks.push(obj);
 			    });
 			    this.set('avnetworks', networks);
 			    this.set('step', 5);
-
-			}, (err) => {
+			  }, (err) => {
 			    let errors = this.get('errors') || [];
 			    errors.pushObject(this.apiErrorMessage(err, '', '', 'WARNING No zone could be found!'));
 			    this.set('errors', errors);
 			    this.set('step', 3);
-			});
-		    },
+			  });
+		  },
 
-		    selectNetwork: function() {
-                        this.set('step', 6);
-			let templatestype = [ { "id": "self", "name": "Private" }, { "id": "featured", "name": "Public" } ];
-                        this.set('avtemplatestype', templatestype);
-                        this.set('step', 7);
-                    },
+		  selectNetwork: function() {
+        this.set('step', 6);
+			  let templatestype = [ { "id": "self", "name": "Private" }, { "id": "featured", "name": "Public" } ];
+        this.set('avtemplatestype', templatestype);
+        this.set('step', 7);
+      },
 
-                    selectTemplateType: function() {
-			this.set('step', 8);
-			this.apiRequest('listTemplates', { templatefilter: this.get('model.%%DRIVERNAME%%Config.templatefilter'), zoneid: this.get('model.%%DRIVERNAME%%Config.zoneid') }).then((res) => {
- 	                    let templates = [];
+      selectTemplateType: function() {
+			  this.set('step', 8);
+			  this.apiRequest('listTemplates', { templatefilter: this.get('model.%%DRIVERNAME%%Config.templatefilter'), zoneid: this.get('model.%%DRIVERNAME%%Config.zoneid') }).then((res) => {
+ 	        let templates = [];
 			    (res.listtemplatesresponse.template || []).forEach((temp) => {
-                                let obj = {
-                                    id: temp.id,
-                                    name: temp.name
-                                };
-                                templates.push(obj);
-                            });
-                            this.set('avtemplates', templates);
-                            this.set('step', 9);
-		        }, (err) => {
-                            let errors = this.get('errors') || [];
-                            errors.pushObject(this.apiErrorMessage(err, '', '', 'No templates could be found!'));
-                            this.set('errors', errors);
-                            this.set('step', 3);
-                        });
-                    },
+            let obj = {
+              id: temp.id,
+              name: temp.name
+            };
+            templates.push(obj);
+          });
+          this.set('avtemplates', templates);
+          this.set('step', 9);
+		    }, (err) => {
+          let errors = this.get('errors') || [];
+          errors.pushObject(this.apiErrorMessage(err, '', '', 'No templates could be found!'));
+          this.set('errors', errors);
+          this.set('step', 3);
+        });
+      },
 
-                    selectTemplate: function() {
-			this.set('step', 10);
-                        this.apiRequest('listServiceOfferings', { issystem: 'false' }).then((res) => {
-                            let serviceofferings = [];
-                            (res.listserviceofferingsresponse.serviceoffering || []).forEach((servoff) => {
-                                let obj = {
-                                    id: servoff.id,
-                                    name: servoff.name
-                                };
-                                serviceofferings.push(obj);
-                            });
-                            this.set('avservofferings', serviceofferings);
-                            this.set('step', 11);
-                        }, (err) => {
-                            let errors = this.get('errors') || [];
-                            errors.pushObject(this.apiErrorMessage(err, '', '', 'No service offerings found!'));
-                            this.set('errors', errors);
-                            this.set('step', 3);
-                        });
-                    },
+      selectTemplate: function() {
+			  this.set('step', 10);
+        this.apiRequest('listServiceOfferings', { issystem: 'false' }).then((res) => {
+          let serviceofferings = [];
+          (res.listserviceofferingsresponse.serviceoffering || []).forEach((servoff) => {
+            let obj = {
+              id: servoff.id,
+              name: servoff.name
+            };
+            serviceofferings.push(obj);
+          });
+          this.set('avservofferings', serviceofferings);
+          this.set('step', 11);
+        }, (err) => {
+          let errors = this.get('errors') || [];
+          errors.pushObject(this.apiErrorMessage(err, '', '', 'No service offerings found!'));
+          this.set('errors', errors);
+          this.set('step', 3);
+        });
+      },
 
-		    selectServiceOffering: function() {
-                        this.set('step', 12);
-                        this.apiRequest('listDiskOfferings').then((res) => {
-                            let diskofferings = [];
+		  selectServiceOffering: function() {
+        this.set('step', 12);
+        this.apiRequest('listDiskOfferings').then((res) => {
+          let diskofferings = [];
 			    diskofferings.push({ "id": null, "name": "No extra disks" });
-                            (res.listdiskofferingsresponse.diskoffering || []).forEach((diskoff) => {
-				if ( diskoff.disksize == 0 ) {
-                            	    let obj = {
-                                	    id: diskoff.id,
-	                                    name: diskoff.name
-        	                        };
-                	             diskofferings.push(obj);
-                            }});
-                            this.set('avdiskofferings', diskofferings);
-                            this.set('step', 13);
-                        }, (err) => {
-                            let errors = this.get('errors') || [];
-                            errors.pushObject(this.apiErrorMessage(err, '', '', 'No disk offerings found!'));
-                            this.set('errors', errors);
-                            this.set('step', 3);
-                        });
-                    },
+          (res.listdiskofferingsresponse.diskoffering || []).forEach((diskoff) => {
+				    if ( diskoff.disksize == 0 ) {
+              let obj = {
+                id: diskoff.id,
+	              name: diskoff.name
+        	    };
+              diskofferings.push(obj);
+            }});
+            this.set('avdiskofferings', diskofferings);
+            this.set('step', 13);
+          }, (err) => {
+            let errors = this.get('errors') || [];
+            errors.pushObject(this.apiErrorMessage(err, '', '', 'No disk offerings found!'));
+            this.set('errors', errors);
+            this.set('step', 3);
+          });
+        },
 
 		    setInstance: function() {
-			this.set('step', 14);
-			this.set('step', 15);
-	  	    }
+			    this.set('step', 14);
+			    this.set('step', 15);
+	  	  }
 		},
 
 		apiRequest: function(command, params) {
-		    let url		= this.get('app.proxyEndpoint') + '/' + this.get('model.%%DRIVERNAME%%Config.apiurl');
-		    params		= params || {};
-		    params.command	= command;
-		    params.apiKey	= this.get('model.%%DRIVERNAME%%Config.apikey');
-                    params.region	= this.get('model.%%DRIVERNAME%%Config.vdcregion');
-		    params.response	= 'json';
+		  let url		= this.get('app.proxyEndpoint') + '/' + this.get('model.%%DRIVERNAME%%Config.apiurl');
+		  params		= params || {};
+		  params.command	= command;
+		  params.apiKey	= this.get('model.%%DRIVERNAME%%Config.apikey');
+      params.region	= this.get('model.%%DRIVERNAME%%Config.vdcregion');
+		  params.response	= 'json';
 
-		    return this.ajaxPromise({url: url, method: 'POST', dataType: 'json',
-							headers: {
-								'Accept': 'application/json',
-								'X-API-Headers-Restrict': 'Content-Length'
-							},
-							beforeSend: (xhr, settings) => {
-								xhr.setRequestHeader('Content-Type', 'rancher:' + settings.contentType);
-								let qs = settings.data.split('&')
-									.map((q) => q.replace(/\+/g, '%20'))
-									.map(Function.prototype.call, String.prototype.toLowerCase)
-									.sort()
-									.join('&');
-									settings.data += '&signature=' + encodeURIComponent(AWS.util.crypto.hmac(
-										this.get('model.%%DRIVERNAME%%Config.secretkey'), qs, 'base64', 'sha1'));
-									return true;
-							},
-							data: params}, true);
+		  return this.ajaxPromise({url: url, method: 'POST', dataType: 'json',
+			  headers: {
+				  'Accept': 'application/json',
+					'X-API-Headers-Restrict': 'Content-Length'
+			  },
+				beforeSend: (xhr, settings) => {
+				  xhr.setRequestHeader('Content-Type', 'rancher:' + settings.contentType);
+					let qs = settings.data.split('&')
+					  .map((q) => q.replace(/\+/g, '%20'))
+					  .map(Function.prototype.call, String.prototype.toLowerCase)
+						.sort()
+						.join('&');
+					settings.data += '&signature=' + encodeURIComponent(AWS.util.crypto.hmac(
+					this.get('model.%%DRIVERNAME%%Config.secretkey'), qs, 'base64', 'sha1'));
+					return true;
+				},
+				data: params}, true);
 		},
 
 		ajaxPromise: function(opt, justBody) {
-		    var promise = new Ember.RSVP.Promise(function(resolve,reject) {
-		    Ember.$.ajax(opt).then(success,fail);
-		    function success(body, textStatus, xhr) {
+		  var promise = new Ember.RSVP.Promise(function(resolve,reject) {
+		  Ember.$.ajax(opt).then(success,fail);
+		  function success(body, textStatus, xhr) {
 			if ( justBody === true ){
-			    resolve(body, 'AJAX Response: '+ opt.url + '(' + xhr.status + ')');
+			  resolve(body, 'AJAX Response: '+ opt.url + '(' + xhr.status + ')');
 			}
 			else {
-			    resolve({xhr: xhr, textStatus: textStatus},'AJAX Response: '+ opt.url + '(' + xhr.status + ')');
+			  resolve({xhr: xhr, textStatus: textStatus},'AJAX Response: '+ opt.url + '(' + xhr.status + ')');
 			}}
 			function fail(xhr, textStatus, err) {
-			    reject({xhr: xhr, textStatus: textStatus, err: err}, 'AJAX Error:' + opt.url + '(' + xhr.status + ')');
+			  reject({xhr: xhr, textStatus: textStatus, err: err}, 'AJAX Error:' + opt.url + '(' + xhr.status + ')');
 			}
 			},'Raw AJAX Request: '+ opt.url);
 		    return promise;
@@ -197,9 +196,9 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 		    let answer	= (err.xhr || {}).responseJSON || {};
 		    let text	= (answer[kind] || {}).errortext;
 			if (text) {
-			    return prefix + ": " + text;
+			  return prefix + ": " + text;
 			} else {
-			    return def;
+			  return def;
 			}
 		},
 
@@ -212,18 +211,18 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 		isStep5: Ember.computed.equal('step', 5),
 		isStep6: Ember.computed.equal('step', 6),
 		isStep7: Ember.computed.equal('step', 7),
-                isStep8: Ember.computed.equal('step', 8),
-                isStep9: Ember.computed.equal('step', 9),
-                isStep10: Ember.computed.equal('step', 10),
-                isStep11: Ember.computed.equal('step', 11),
+    isStep8: Ember.computed.equal('step', 8),
+    isStep9: Ember.computed.equal('step', 9),
+    isStep10: Ember.computed.equal('step', 10),
+    isStep11: Ember.computed.equal('step', 11),
 		isStep12: Ember.computed.equal('step', 12),
 		isStep13: Ember.computed.equal('step', 13),
-                isStep14: Ember.computed.equal('step', 14),
-                isStep15: Ember.computed.equal('step', 15),
+    isStep14: Ember.computed.equal('step', 14),
+    isStep15: Ember.computed.equal('step', 15),
 		isGteStep3: Ember.computed.gte('step', 3),
 		isGteStep5: Ember.computed.gte('step', 5),
 		isGteStep7: Ember.computed.gte('step', 7),
-                isGteStep9: Ember.computed.gte('step', 9),
+    isGteStep9: Ember.computed.gte('step', 9),
 		isGteStep11: Ember.computed.gte('step', 11),
 		isGteStep13: Ember.computed.gte('step', 13),
 		isGteStep15: Ember.computed.gte('step', 15),
